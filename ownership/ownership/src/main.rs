0ff183fc3eb32a9bc18cc2d1ab696e20c2d37706
fenghaojiang <fenghaojiang97@m.scnu.edu.cn>
2022-06-12 23:24:54 +0800

    update

--- ownership/ownership/src/main.rs
+++ ownership/ownership/src/main.rs
@@ -1,6 +1,12 @@
-fn main() {
+fn main1() {
     let mut s = String::from("hello");
     s.push_str(", world!");
     println!("{}", s);
 }
 
+
+fn main() {
+    let x: &str = "hello, world";
+    let y = x;
+    println!("{},{}",x,y);
+}
